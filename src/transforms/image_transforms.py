a7a4505bbe7c4f9a63ca7fc4279ffcb18d7b7f6d
Fae <this.fairuz@gmail.com>
2024-10-22 16:17:48 +0700

    Splt transformer for train and test data

--- src/transforms/image_transforms.py
+++ src/transforms/image_transforms.py
@@ -1,8 +1,17 @@
 import torchvision.transforms as transforms
 
-transformer = transforms.Compose([
+train_transformer = transforms.Compose([
     transforms.Resize((64, 64)),  
     transforms.RandomHorizontalFlip(),  
+    transforms.RandomRotation(10),  
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  
+    transforms.ToTensor(),  
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  
+])
+
+# Gapake augmentation
+test_transformer = transforms.Compose([
+    transforms.Resize((64, 64)),  
     transforms.ToTensor(),  
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  
 ])
