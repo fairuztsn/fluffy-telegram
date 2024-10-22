dad3a6deddc4ba29702ebee6b8f695fcba319782
Fae <this.fairuz@gmail.com>
2024-10-22 15:54:43 +0700

    Resolved label structure issue: added jenis and warna indexing

--- src/datasets/custom_dataset.py
+++ src/datasets/custom_dataset.py
@@ -9,16 +9,25 @@ class CustomImageDatasetFromCSV(Dataset):
         self.data_frame = pd.read_csv(csv_file)  # Membaca file CSV
         self.img_dir = img_dir  # Direktori gambar
         self.transform = transform  # Transformasi yang akan diterapkan pada gambar
+        
+        # Mengambil kelas unik dari kombinasi 'jenis' dan 'warna'
         self.classes = sorted(self.data_frame[['jenis', 'warna']].drop_duplicates().apply(tuple, axis=1))
         self.class_to_idx = {cls: idx for idx, cls in enumerate(self.classes)}
+        
+        # Membuat mapping untuk jenis dan warna secara terpisah
+        self.jenis_classes = sorted(self.data_frame['jenis'].unique())
+        self.warna_classes = sorted(self.data_frame['warna'].unique())
+        self.jenis_to_idx = {jenis: idx for idx, jenis in enumerate(self.jenis_classes)}
+        self.warna_to_idx = {warna: idx for idx, warna in enumerate(self.warna_classes)}
 
     def __len__(self):
         return len(self.data_frame)  # Mengembalikan jumlah data
 
     def __getitem__(self, idx):
         img_id = self.data_frame.iloc[idx, 0]  # Mengambil ID gambar dari kolom pertama
-        label = self.data_frame.iloc[idx, 1]  # Mengambil label dari kolom kedua
-
+        jenis = self.data_frame.iloc[idx, 1]  # Mengambil 'jenis' dari kolom kedua
+        warna = self.data_frame.iloc[idx, 2]  # Mengambil 'warna' dari kolom ketiga
+        
         # Cek keberadaan gambar dengan dua format: .jpg dan .png
         img_path_jpg = os.path.join(self.img_dir, f"{img_id}.jpg")
         img_path_png = os.path.join(self.img_dir, f"{img_id}.png")
@@ -37,4 +46,9 @@ class CustomImageDatasetFromCSV(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        return image, label  # Mengembalikan gambar dan label
+        # Mengubah label jenis dan warna menjadi indeks
+        jenis_idx = self.jenis_to_idx[jenis]
+        warna_idx = self.warna_to_idx[warna]
+
+        # Mengembalikan image dan tuple (jenis_idx, warna_idx) sebagai tensor
+        return image, torch.tensor([jenis_idx, warna_idx], dtype=torch.long)
